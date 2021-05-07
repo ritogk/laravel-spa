4e139ff3cb89d784906952ec7eb3e292310547a1
ryoji <homing_job@yahoo.co.jp>
2021-05-07 12:22:09 +0900

    仕事マスタ migration作成

---
+++ project/database/migrations/2021_05_07_121524_create_job.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJob extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->text('Content');
+            $table->boolean('attention');
+            $table->integer('job_category_id');
+            $table->bigInteger('price');
+            $table->string('image');
+            $table->integer('sort_no');
+            $table->timestamps();
+            $table->softDeletes()->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job');
+    }
+}
