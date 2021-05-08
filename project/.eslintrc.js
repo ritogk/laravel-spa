bbed38ac8af6403a67c5ba0de688508ddd712601
ryoji <homing_job@yahoo.co.jp>
2021-05-09 08:08:04 +0900

    vscodeでvueファイルを開いた際の謎エラーを解決。※根本的な理由は分かってない

--- project/.eslintrc.js
+++ project/.eslintrc.js
@@ -1,22 +1,30 @@
 module.exports = {
+    root: true,
     "env": {
         "browser": true,
         "commonjs": true,
         "es2021": true
     },
     "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential",
-        "plugin:@typescript-eslint/recommended"
+        'plugin:vue/essential',
+        'eslint:recommended',
+        '@vue/typescript/recommended',
+        '@vue/prettier',
+        '@vue/prettier/@typescript-eslint',
     ],
     "parserOptions": {
         "ecmaVersion": 12,
-        "parser": "@typescript-eslint/parser"
+        "sourceType": "module"
+        //"parser": "@typescript-eslint/parser",
+        //"extends": "eslint:recommended",
+        //"parser": "vue-eslint-parser"
     },
     "plugins": [
         "vue",
         "@typescript-eslint"
     ],
     "rules": {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     }
 };
