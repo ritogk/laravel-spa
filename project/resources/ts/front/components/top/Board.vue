1b022d35bf00e459beb702e58d4634fc96882a5a
ryoji <homing_job@yahoo.co.jp>
2021-05-12 09:20:10 +0900

    カテゴリコンポーネント内で全カテゴリを表示するように変更

--- project/resources/ts/front/components/top/Board.vue
+++ project/resources/ts/front/components/top/Board.vue
@@ -6,15 +6,7 @@
             sub-title='ご希望の職種を選択して下さい。'
             tag="article"
         >
-
-        <b-card-group deck>
-            <app-category
-            v-for="(category, index) in categories"
-            :key="`category-${index}`"
-            :category="category"
-            @click.native='pageTransition()'
-            ></app-category>
-        </b-card-group>
+            <app-category></app-category>
         </b-card>
     </div>
 </template>
@@ -23,11 +15,6 @@
     import { Vue, Component, Watch } from 'vue-property-decorator';
     // コンポーネント
     import Category from './Category.vue';
-    // モデル
-    import ICond from "@root/front/models/ICond";
-    import ICategory from "@root/front/models/ICategory";
-    // 状態管理
-    import { state } from "@root/front/state";
 
     @Component({
         components: {
@@ -35,19 +22,5 @@
         }
     })
     export default class App extends Vue {
-        categories: Array<ICategory> = []
-
-        pageTransition(){
-            const cond: ICond = {category: '1', content: '', price: null, attention: false}
-            state.searchJob(cond)
-            this.$router.push({ name: 'main'})
-        }
-
-        // 初期化
-        mounted(){
-            window.axios.post('/front/categories').then(response => {
-                this.categories = response.data
-            })
-        }
     }
 </script>
--- project/resources/ts/front/components/top/Category.vue
+++ project/resources/ts/front/components/top/Category.vue
@@ -1,32 +1,55 @@
 <template>
-    <div>
-        <b-card
-            :title='category.name'
-            :img-src='category.image'
-            img-alt="Image"
-            img-top
-            img-height="200px"
-            text-variant="white"
-            tag="article"
-            class="mb-4"
-            style="max-width: 20rem;"
-            overlay
-        >
-            <b-card-text>
-                <span v-text='category.content'></span>
-                <a href="#" @click.prevent="" class="stretched-link"></a>
-            </b-card-text>
-        </b-card>
-    </div>
+    <b-card-group deck>
+        <div
+            v-for="(category, index) in categories"
+            :key="`category-${index}`"
+            :category="category"
+            @click.native='pageTransition()'
+            >
+            <b-card
+                :title='category.name'
+                :img-src='category.image'
+                img-alt="Image"
+                img-top
+                img-height="200px"
+                text-variant="white"
+                tag="article"
+                class="mb-4"
+                style="max-width: 20rem;"
+                overlay
+            >
+                <b-card-text>
+                    <span v-text='category.content'></span>
+                    <a href="#" @click.prevent="pageTransition()" class="stretched-link"></a>
+                </b-card-text>
+            </b-card>
+        </div>
+    </b-card-group>
 </template>
 
 <script lang="ts">
     import { Vue, Component, Prop, Emit } from 'vue-property-decorator';
     // モデル
+    import ICond from "@root/front/models/ICond";
     import ICategory from "@root/front/models/ICategory";
+    // 状態管理
+    import { state } from "@root/front/state";
+
     @Component
     export default class Detail extends Vue {
-        @Prop({required: true })
-        category!: ICategory;
+        categories: Array<ICategory> = []
+
+        // 初期化
+        mounted(){
+            window.axios.post('/front/categories').then(response => {
+                this.categories = response.data
+            })
+        }
+
+        pageTransition(){
+            const cond: ICond = {category: '1', content: '', price: null, attention: false}
+            state.searchJob(cond)
+            this.$router.push({ name: 'main'})
+        }
     }
 </script>
