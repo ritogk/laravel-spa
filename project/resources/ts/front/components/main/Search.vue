744d7109ecde3b90455ca81d0b6f34c5b5ed81b1
ryoji <homing_job@yahoo.co.jp>
2021-05-12 11:40:07 +0900

    求人検索コンポーネント 見た目をかっこ良く修正

--- project/resources/ts/front/components/main/Search.vue
+++ project/resources/ts/front/components/main/Search.vue
@@ -3,34 +3,44 @@
         <div class="sticky-top">
             <div class="nav flex-column">
                 <b-card-group deck>
-                    <b-card header="検索" header-tag="header">
-                        <div class="py-2">
-                            <label for="exampleInputEmail1">職種</label>
-                            <b-form-select :options="[1, 2, 3, 4, 5]" v-model="category"></b-form-select>
+                    <b-card
+                        border-variant="info"
+                        header-bg-variant="primary"
+                        header-text-variant="white"
+                        header="検索"
+                        header-tag="header"
+                        class="mb-3"
+                        no-body
+                    >
+                        <div class="m-3">
+                            <div class="py-2">
+                                <label for="exampleInputEmail1">職種</label>
+                                <b-form-select :options="[1, 2, 3, 4, 5]" v-model="category"></b-form-select>
 
-                        </div>
+                            </div>
 
-                        <div class="py-2">
-                            <label for="exampleInputEmail1">仕事内容</label>
-                            <b-form-input placeholder="どんな求人をお探しですか?" v-model="content"></b-form-input>
-                        </div>
+                            <div class="py-2">
+                                <label for="exampleInputEmail1">仕事内容</label>
+                                <b-form-input placeholder="どんな求人をお探しですか?" v-model="content"></b-form-input>
+                            </div>
 
-                        <div class="py-2">
-                            <label for="exampleInputEmail1">金額</label>
-                            <b-form-input type="number" placeholder="最低金額を入力して下さい。" v-model="price"></b-form-input>
-                        </div>
+                            <div class="py-2">
+                                <label for="exampleInputEmail1">金額</label>
+                                <b-form-input type="number" placeholder="最低金額を入力して下さい。" v-model="price"></b-form-input>
+                            </div>
 
-                        <div class="py-2">
-                            <b-form-checkbox
-                            v-model="attention"
-                            id="checkbox-1"
-                            name="checkbox-1">
-                            注目の求人
-                            </b-form-checkbox>
-                        </div>
+                            <div class="py-2">
+                                <b-form-checkbox
+                                v-model="attention"
+                                id="checkbox-1"
+                                name="checkbox-1">
+                                注目の求人
+                                </b-form-checkbox>
+                            </div>
 
-                        <div class="py-2">
-                            <b-button variant="primary" @click="search()">検索</b-button>
+                            <div class="py-2">
+                                <b-button variant="primary" @click="search()">検索</b-button>
+                            </div>
                         </div>
                     </b-card>
                 </b-card-group>
