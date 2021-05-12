e3a5082013475f783dfb1fb1339fff00e3651309
ryoji <homing_job@yahoo.co.jp>
2021-05-12 11:48:29 +0900

    カテゴリ選択コンポーネント 見た目をそれっぽく修正

--- project/resources/ts/front/components/top/Board.vue
+++ project/resources/ts/front/components/top/Board.vue
@@ -1,13 +1,6 @@
 <template>
     <div class="p-4">
-        <b-card
-            header="職種"
-            header-tag="header"
-            sub-title='ご希望の職種を選択して下さい。'
-            tag="article"
-        >
-            <app-category></app-category>
-        </b-card>
+        <app-category></app-category>
     </div>
 </template>
 
--- project/resources/ts/front/components/top/Category.vue
+++ project/resources/ts/front/components/top/Category.vue
@@ -1,29 +1,39 @@
 <template>
-    <b-card-group deck>
-        <div
-            v-for="(category, index) in categories"
-            :key="`category-${index}`"
-            :category="category"
-        >
-            <b-card
-                :title='category.name'
-                :img-src='category.image'
-                img-alt="Image"
-                img-top
-                img-height="200px"
-                text-variant="white"
-                tag="article"
-                class="mb-4"
-                style="max-width: 20rem;"
-                overlay
+    <b-card
+        border-variant="info"
+        header-bg-variant="primary"
+        header-text-variant="white"
+        header="職種"
+        header-tag="header"
+        sub-title='ご希望の職種を選択して下さい。'
+        class="mb-3"
+    >
+        <b-card-group deck>
+            <div
+                v-for="(category, index) in categories"
+                :key="`category-${index}`"
+                :category="category"
             >
-                <b-card-text>
-                    <span v-text='category.content'></span>
-                    <a href="#" @click.prevent="pageTransition(category)" class="stretched-link"></a>
-                </b-card-text>
-            </b-card>
-        </div>
-    </b-card-group>
+                <b-card
+                    :title='category.name'
+                    :img-src='category.image'
+                    img-alt="Image"
+                    img-top
+                    img-height="200px"
+                    text-variant="white"
+                    tag="article"
+                    class="mb-4"
+                    style="max-width: 20rem;"
+                    overlay
+                >
+                    <b-card-text>
+                        <span v-text='category.content'></span>
+                        <a href="#" @click.prevent="pageTransition(category)" class="stretched-link"></a>
+                    </b-card-text>
+                </b-card>
+            </div>
+        </b-card-group>
+    </b-card>
 </template>
 
 <script lang="ts">
