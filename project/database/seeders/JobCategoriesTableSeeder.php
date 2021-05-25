ac60282ca0ed835706a69d8d4304e3e7fd4e7bd3
homing-jon2 <kxlblvfo@yahoo.co.jp>
2021-05-25 18:18:30 +0900

    seederの画像パスを変更

--- project/database/seeders/JobCategoriesTableSeeder.php
+++ project/database/seeders/JobCategoriesTableSeeder.php
@@ -16,10 +16,10 @@ class JobCategoriesTableSeeder extends Seeder
     public function run()
     {
         $now = Carbon::now();
-        DB::table('job_categories')->insert(['name' => '公務員', 'content' => '役所、消防署、官僚、国税専門官、財務専門官、自衛隊', 'image' => 'https://www.pakutaso.com/shared/img/thumb/PAKSdwe84_TP_V4.jpg', 'sort_no' => 1, 'updated_at' => $now, 'created_at' => $now]);
-        DB::table('job_categories')->insert(['name' => '営業', 'content' => 'コンサルティングファーム、監査法人、専門事務所', 'image' => 'https://www.pakutaso.com/shared/img/thumb/kusaiyoIMGL7225_TP_V.jpg', 'sort_no' => 2, 'updated_at' => $now, 'created_at' => $now]);
-        DB::table('job_categories')->insert(['name' => 'IT', 'content' => 'アプリ開発、インフラ、ITコンサルタント、社内SE、ヘルプデスク、データサイエンティスト', 'image' => 'https://www.pakutaso.com/shared/img/thumb/macbook1696_TP_V.jpg', 'sort_no' => 3, 'updated_at' => $now, 'created_at' => $now]);
-        DB::table('job_categories')->insert(['name' => '家', 'content' => '建築／土木／不動産／プラント／設備', 'image' => 'https://www.pakutaso.com/shared/img/thumb/MIYA19224DSC_7993_TP_V.jpg', 'sort_no' => 4, 'updated_at' => $now, 'created_at' => $now]);
-        DB::table('job_categories')->insert(['name' => '小売店', 'content' => 'スーパー/コンビニ', 'image' => 'https://www.pakutaso.com/shared/img/thumb/hawaiiDSC00241_TP_V.jpg', 'sort_no' => 5, 'updated_at' => $now, 'created_at' => $now]);
+        DB::table('job_categories')->insert(['name' => '公務員', 'content' => '役所、消防署、官僚、国税専門官、財務専門官、自衛隊', 'image' => 'public/images/PAKSdwe84_TP_V4.jpg', 'sort_no' => 1, 'updated_at' => $now, 'created_at' => $now]);
+        DB::table('job_categories')->insert(['name' => '営業', 'content' => 'コンサルティングファーム、監査法人、専門事務所', 'image' => 'public/images/kusaiyoIMGL7225_TP_V.jpg', 'sort_no' => 2, 'updated_at' => $now, 'created_at' => $now]);
+        DB::table('job_categories')->insert(['name' => 'IT', 'content' => 'アプリ開発、インフラ、ITコンサルタント、社内SE、ヘルプデスク、データサイエンティスト', 'image' => 'public/images/macbook1696_TP_V.jpg', 'sort_no' => 3, 'updated_at' => $now, 'created_at' => $now]);
+        DB::table('job_categories')->insert(['name' => '家', 'content' => '建築／土木／不動産／プラント／設備', 'image' => 'public/images/MIYA19224DSC_7993_TP_V.jpg', 'sort_no' => 4, 'updated_at' => $now, 'created_at' => $now]);
+        DB::table('job_categories')->insert(['name' => '小売店', 'content' => 'スーパー/コンビニ', 'image' => 'public/images/hawaiiDSC00241_TP_V.jpg', 'sort_no' => 5, 'updated_at' => $now, 'created_at' => $now]);
     }
 }
