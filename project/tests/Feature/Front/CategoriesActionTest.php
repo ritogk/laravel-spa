02432f697e072acd021b1376063c6efbd52431cc
homing-jon2 <kxlblvfo@yahoo.co.jp>
2021-05-27 08:45:15 +0900

    仕事カテゴリ取得api test作成

---
+++ project/tests/Feature/Front/CategoriesActionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\Front;
+
+use Tests\TestCase;
+use App\UseCases\Front\CategoriesAction;
+
+class CategoriesActionTest extends TestCase
+{
+    /**
+     * 200 test
+     *
+     * @return void
+     */
+    public function test_200()
+    {
+        // アクセス
+        $response = $this->post('/front/categories');
+        // データ取得
+        $action =  new CategoriesAction();
+        // チェック
+        $response->assertStatus(200)
+                ->assertJson($action());
+
+    }
+}
