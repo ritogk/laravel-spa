6b17e818e7f10c6a887dbe5daccd217bf9c1dd42
伊藤遼二 <r_itou@ranasoft.co.jp>
2021-05-31 23:11:51 +0900

    環境変数を見て強制sslを制御

---
+++ project/app/Http/Middleware/ForceHttps.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ForceHttps
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (\App::environment(['production']) && $_SERVER["HTTP_X_FORWARDED_PROTO"] != 'https') {
+            return redirect()->secure($request->getRequestUri());
+        }
+        return $next($request);
+    }
+}
