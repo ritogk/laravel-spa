88e546322bcff0677d479c1bdcc1ae19c31ba81b
r.ito <homing0321r4cfw@yahoo.co.jp>
2021-08-01 13:52:07 +0900

    api 一般新規登録 未使用関数削除

--- project/app/Http/Controllers/Api/FrontRegisterController.php
+++ project/app/Http/Controllers/Api/FrontRegisterController.php
@@ -50,21 +50,6 @@ class FrontRegisterController extends Controller
     }
 
     /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
-    }
-
-    /**
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
