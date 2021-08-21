d0a1f6730244d658b6332e3e0bf59f0701fca65b
homing <homing0321r4cfw@yahoo.co.jp>
2021-08-21 16:39:01 +0900

    不具合修正

--- project/app/UseCases/Master/Job/ListAction.php
+++ project/app/UseCases/Master/Job/ListAction.php
@@ -31,7 +31,7 @@ class ListAction{
                     ->toArray();
 
         // ファイルのurlをセット
-        if(count($items) > 1 && array_key_exists('image', $items[0])){
+        if(count($items) >= 1 && array_key_exists('image', $items[0])){
             foreach ($items as &$item) {
                 $item['image_url'] = Storage::url($item['image']);
             }
