19a906ceec3446b74330a1897756cc2bdc22e551
homing <homing0321r4cfw@yahoo.co.jp>
2021-08-22 08:24:08 +0900

    不要なトランザクション削除

--- project/app/UseCases/Master/Job/CreateAction.php
+++ project/app/UseCases/Master/Job/CreateAction.php
@@ -3,8 +3,6 @@ namespace App\UseCases\Master\Job;
 
 use App\Models\Job;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
 
 class CreateAction{
     /**
@@ -15,19 +13,17 @@ class CreateAction{
      */
     public function __invoke(Request $request): void
     {
-        DB::transaction(function () use($request){
-            $create = [
-                'title' => $request->title,
-                'content' => $request->content,
-                'attention' => $request->attention,
-                'job_category_id' => $request->job_category_id,
-                'price' => $request->price,
-                'welfare' => $request->welfare,
-                'holiday' => $request->holiday,
-                'image' => $request->image,
-                'sort_no' => $request->sort_no,
-            ];
-            Job::create($create);
-        });
+        $create = [
+            'title' => $request->title,
+            'content' => $request->content,
+            'attention' => $request->attention,
+            'job_category_id' => $request->job_category_id,
+            'price' => $request->price,
+            'welfare' => $request->welfare,
+            'holiday' => $request->holiday,
+            'image' => $request->image,
+            'sort_no' => $request->sort_no,
+        ];
+        Job::create($create);
     }
 }
