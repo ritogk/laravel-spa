0bdfdc4b845c857573c81ccc83b002ebb2d7863b
homing <homing0321r4cfw@yahoo.co.jp>
2021-08-21 15:21:02 +0900

    職種マスタ一覧取得api path変換処理もれ

--- project/app/UseCases/Master/JobCategory/ListAction.php
+++ project/app/UseCases/Master/JobCategory/ListAction.php
@@ -29,7 +29,7 @@ class ListAction{
         // storageパス変換
         if(count($items) > 1 && array_key_exists('image', $items[0])){
             foreach ($items as &$item) {
-                $item['image'] = Storage::url($item['image']);
+                $item['image_url'] = Storage::url($item['image']);
             }
         }
         return $items;
