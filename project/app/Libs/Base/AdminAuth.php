a0b0e2e9dc7645ca0cb65f853c836f11538f9140
homing <homing0321r4cfw@yahoo.co.jp>
2021-08-23 15:08:04 +0900

    不要コード削除

--- project/app/Libs/Base/AdminAuth.php
+++ project/app/Libs/Base/AdminAuth.php
@@ -1,11 +1,8 @@
 <?php
 namespace App\Libs\Base;
 
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Requests\Auth\Admin\LoginRequest;
 
 trait AdminAuth
 {
