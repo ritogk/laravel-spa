c62cb1f62c3632809c9d3a17da7b8d0211b66c30
root <homing-job@yahoo.co.jp>
2022-01-07 18:59:45 +0900

    remote-container系

--- project/setup.sh
+++ project/setup.sh
@@ -7,3 +7,6 @@ php artisan migrate:refresh --seed
 chmod -R 777 storage
 chmod -R 777 bootstrap/cache
 php artisan storage:link
+wget https://github.com/homing-job/laravel-spa/files/6542800/seeder_images.zip
+unzip seeder_images.zip -d storage/app/public/images
+rm -f seeder_images.zip
